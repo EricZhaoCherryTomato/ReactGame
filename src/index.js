4492c3b5f53d16c2119c179a91828361b04e98a1
Eric Zhao <eric.zhao@virginaustralia.com>
2019-07-26 16:23:57 +1000

    function componenet change on Square

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-class Square extends React.Component {
-    render() {
-        const i = this.props.value;
-        return (
-            <button
-                className="square"
-                onClick={()=> this.props.onClick()}
-            >
-                {i}
-            </button>
-        );
-    }
-  }
+function Square(props) {
+    return (
+        <button className="square"
+                onClick={props.onClick}
+        >{props.value}</button>
+    );
+}
 
   class Board extends React.Component {
       constructor(props){
