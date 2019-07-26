b8a2a885190d2e041ff88bdbadfce2ca5acd597e
Eric Zhao <eric.zhao@virginaustralia.com>
2019-07-26 10:29:28 +1000

    Updates

---
+++ precache-manifest.0f9b6b4cca29398c85d4b0b2dfbf8df9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc27c82ebd65da9069fbeb6b616b9526",
+    "url": "/ReactGame/index.html"
+  },
+  {
+    "revision": "307854315fc731d9c4ad",
+    "url": "/ReactGame/static/css/main.3502e27f.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/ReactGame/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "307854315fc731d9c4ad",
+    "url": "/ReactGame/static/js/main.84644f13.chunk.js"
+  },
+  {
+    "revision": "77e357886314402729a3",
+    "url": "/ReactGame/static/js/runtime~main.d5ea2d59.js"
+  }
+]);
